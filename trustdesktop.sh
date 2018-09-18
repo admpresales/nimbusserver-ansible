dffa2a1307e3d10755b7970283d5dfaac53a3af4
Jason Hrabi <jason.hrabi@microfocus.com>
2018-09-18 11:13:49 -0400

    Added mimeapps.list for default browser settings Added monitors.xml for default monitor settings in VM Updated quickstart documentation. Created initial flag for nimbus

--- trustdesktop.sh
+++ trustdesktop.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-for i in ~/Desktop/*.desktop; do
-    echo $i
-    gio set $i "metadata::trusted" yes
-done
+if [ ! -f ~/.config/nimbus-initial-done ]; then
+
+    for i in ~/Desktop/*.desktop; do
+        echo $i
+        gio set $i "metadata::trusted" yes
+    done
+
+    #Launch chrome on initial startup to see NimbusServer details
+    /usr/bin/google-chrome-stable --password-store=basic %U --silent-debugger-extension-api
+
+    touch ~/.config/nimbus-initial-done
+fi
