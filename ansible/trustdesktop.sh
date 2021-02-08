711273e062076b511f560fac0b942ae641a5f861
Jason Corlett <jasoncorlett@users.noreply.github.com>
2021-02-08 17:32:30 -0500

    Use new value for gnome shell

--- ansible/trustdesktop.sh
+++ ansible/trustdesktop.sh
@@ -9,7 +9,7 @@ if [ ! -f ~/.config/nimbus-initial-done ]; then
     # Trust desktop icons
     for i in ~/Desktop/*.desktop; do
         echo $i
-        gio set $i "metadata::trusted" yes
+        gio set $i "metadata::trusted" true
     done
 
     #Launch chrome on initial startup to see NimbusServer details
