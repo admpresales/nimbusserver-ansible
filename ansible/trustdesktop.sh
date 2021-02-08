ecda96fdcd86ef01ace8c7886511d6e2d95366ef
Jason Corlett <jasoncorlett@users.noreply.github.com>
2021-02-08 16:39:49 -0500

    Enable desktop icons as part of the first run script

--- ansible/trustdesktop.sh
+++ ansible/trustdesktop.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 if [ ! -f ~/.config/nimbus-initial-done ]; then
+    # Disable animations and enable the desktop icons extension
+    gsettings set org.gnome.desktop.interface enable-animations false
+    gsettings set org.gnome.shell disable-user-extensions false
+    gsettings set org.gnome.shell enabled-extensions "['desktop-icons@gnome-shell-extensions.gcampax.github.com']"
 
+    # Trust desktop icons
     for i in ~/Desktop/*.desktop; do
         echo $i
         gio set $i "metadata::trusted" yes
